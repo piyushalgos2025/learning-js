e0d390a6038feeb729ed23dd344500be70a51bfa
piyushalgos2025 <piyushjain92114211@gmail.com>
2025-06-18 08:46:41 +0000

    done2

---
+++ second.js
@@ -0,0 +1,14 @@
+let a = 15;
+let b = 5;
+
+let sum = a + b;
+let diff = a - b;
+let product = a * b;
+let quotient = a / b;
+
+console.log("a =", a);
+console.log("b =", b);
+console.log("Addition:", sum);
+console.log("Subtraction:", diff);
+console.log("Multiplication:", product);
+console.log("Division:", quotient);
